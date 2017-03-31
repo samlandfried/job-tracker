3dd1c5267d8581ff50d927b8dcd8d544f26294a3
Sam Landfried <samlandfried@gmail.com>
2017-03-30 18:04:05 -0600

    Add update functionality to jobs from show page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,11 +25,15 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+
+    @job.update(job_params)
+
+    redirect_to company_job_path(@job.company, @job)
   end
 
   def destroy
