4ee76c9b5951199e9df72b105b5671c7e813e643
Sam Landfried <samlandfried@gmail.com>
2017-04-01 15:40:55 -0600

    Add #companies_with_most_interest in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ require './app/models/job_dashboard'
 
 class DashboardController < ApplicationController
   def index
-    @dashboard = JobDashboard.new(Job.all)
+    @dashboard = JobDashboard.new
   end
 end
--- app/models/job_dashboard.rb
+++ app/models/job_dashboard.rb
@@ -1,12 +1,20 @@
 class JobDashboard
 
-  attr_reader :jobs
+  attr_reader :jobs, :companies
 
-  def initialize(jobs)
-    @jobs = jobs
+  def initialize
+    @jobs = Job.all
+    @companies = Company.all
   end
 
   def number_of_jobs_by_level_of_interest
-    jobs.order(:level_of_interest).group(:level_of_interest).count
+    Job.order(:level_of_interest).group(:level_of_interest).count
+  end
+
+  def companies_with_most_interest(n = 1)
+    company_interest = Job.group(:company_id).average(:level_of_interest)
+    company_interest.sort_by do |company_id, avg_interest|
+      -avg_interest
+    end.first(n).map {|tuple| [Company.find(tuple.first), tuple.last.to_i]}
   end
 end
