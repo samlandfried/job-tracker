c2861977241b171edd53d4787b3afecf079e7a5e
Sam Landfried <samlandfried@gmail.com>
2017-04-01 10:58:32 -0600

    Add full jobs index

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -18,8 +18,9 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    company = Company.find(params[:id])
-    redirect_to company_jobs_path(company)
+    @company = Company.find(params[:id])
+    @contacts = @company.contacts
+    @jobs = @company.jobs
   end
 
   def edit
